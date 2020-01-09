506a2d6111cfb9eb13d962ef05faabbc4c3342a2
Ann Said <ann.s.eit@gmail.com>
2020-01-09 22:45:47 +0200

    Updates

---
+++ precache-manifest.e022afccf13291469c3c381d823af989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc82bb5eea7d15affca71eeeefa48e2",
+    "url": "/mymovies-react-redux/index.html"
+  },
+  {
+    "revision": "9a620436e80056886320",
+    "url": "/mymovies-react-redux/static/css/main.df0141f0.chunk.css"
+  },
+  {
+    "revision": "1f40173f5a46d27ecfea",
+    "url": "/mymovies-react-redux/static/js/2.4b3fece1.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/mymovies-react-redux/static/js/2.4b3fece1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a620436e80056886320",
+    "url": "/mymovies-react-redux/static/js/main.a7010266.chunk.js"
+  },
+  {
+    "revision": "26712d12d29235ececdb",
+    "url": "/mymovies-react-redux/static/js/runtime-main.ed3970e4.js"
+  }
+]);
