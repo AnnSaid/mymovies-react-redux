342867a02ab002f2561bee5bec9e95e511026c44
Ann Said <ann.s.eit@gmail.com>
2020-01-10 14:10:31 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(){
         <div className="container-fluid">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <Link className="navbar-brand name" to="/">
-              MyMovie
+              MyMovies
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
